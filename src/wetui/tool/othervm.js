334baa2dc0549f8d7f6d30e489047ac7020710f3
v_jeetang <v_jeetang@tencent.com>
2020-10-31 22:55:59 +0800

    首屏loading加载改变

--- src/wetui/tool/othervm.js
+++ src/wetui/tool/othervm.js
@@ -2,23 +2,20 @@ import { createApp, h } from "vue";
 
 function othervm(component, props) {
     //这里就是未来渲染 alert message tips的其他渲染盒
-    const otherNodeBox = document.querySelector('.wetothernodebox');
-    if (!otherNodeBox) {
-        const body = document.getElementsByTagName('body')[0];
-        const otherNodeBox = document.createElement('div');
-        otherNodeBox.style.position = "absolute";
-        otherNodeBox.setAttribute('class', 'wetothernodebox');
-        body.appendChild(otherNodeBox);
-    }
+    const body = document.getElementsByTagName('body')[0];
+    const otherNodeBox = document.createElement('div');
+    otherNodeBox.style.position = "absolute"
+    body.appendChild(otherNodeBox);
     const vm = createApp({
         render() {
             return h(component, props)
         }
     })
     const vmmount = vm.mount(otherNodeBox);
+    //console.log(vmmount.$el.scrollTop)
     vmmount.$removeVmNode = () => {
         vm.unmount(otherNodeBox);
-        //body.removeChild(otherNodeBox)
+        body.removeChild(otherNodeBox)
     }
     return vmmount.$el
 }
