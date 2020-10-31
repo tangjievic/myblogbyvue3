42f742d3aea08f9089039338b07b3b9f7805307c
v_jeetang <v_jeetang@tencent.com>
2020-10-31 14:01:08 +0800

    提交

--- src/wetui/tool/othervm.js
+++ src/wetui/tool/othervm.js
@@ -2,9 +2,14 @@ import { createApp, h } from "vue";
 
 function othervm(component, props) {
     //这里就是未来渲染 alert message tips的其他渲染盒
-    const body = document.getElementsByTagName('body')[0];
-    const otherNodeBox = document.createElement('div');
-    body.appendChild(otherNodeBox);
+    const otherNodeBox = document.querySelector('.wetothernodebox');
+    if (!otherNodeBox) {
+        const body = document.getElementsByTagName('body')[0];
+        const otherNodeBox = document.createElement('div');
+        otherNodeBox.style.position = "absolute";
+        otherNodeBox.setAttribute('class', 'wetothernodebox');
+        body.appendChild(otherNodeBox);
+    }
     const vm = createApp({
         render() {
             return h(component, props)
@@ -14,7 +19,7 @@ function othervm(component, props) {
     //console.log(vmmount.$el.scrollTop)
     vmmount.$removeVmNode = () => {
         vm.unmount(otherNodeBox);
-        body.removeChild(otherNodeBox)
+        //body.removeChild(otherNodeBox)
     }
     return vmmount.$el
 }
